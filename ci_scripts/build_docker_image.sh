c7005221523f97f2ae089e4ba347543b3d0a9436
viktor zhelev <viktorzhelev1@gmail.com>
2021-01-10 20:37:06 +0200

    aide

--- ci_scripts/build_docker_image.sh
+++ ci_scripts/build_docker_image.sh
@@ -1,23 +1,20 @@
 #!/bin/bash -e
 
-image_name=user5-web-app
-ECR=378652049675.dkr.ecr.eu-west-1.amazonaws.com
-
 echo "logging to ECR"
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin $ECR
-
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 378652049675.dkr.ecr.eu-west-1.amazonaws.com
 
 echo "building docker image"
-docker build -t $image_name .
+docker build -t user2-web-app .
+
 
 
 echo "tagging the built docker image with the repo address"
-docker tag $image_name $ECR/$image_name:latest
+docker tag user2-web-app:latest 378652049675.dkr.ecr.eu-west-1.amazonaws.com/user2-web-app:latest
 
 
 
 echo "pushing docker image to ECR"
-docker push $ECR/$image_name:latest
+docker push 378652049675.dkr.ecr.eu-west-1.amazonaws.com/user2-web-app:latest
 
 
 
