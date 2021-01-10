5d72845d59c7322ee46e261cd696af9b7155fb8f
viktor zhelev <viktorzhelev1@gmail.com>
2021-01-10 20:40:20 +0200

    dsada

--- ci_scripts/build_docker_image.sh
+++ ci_scripts/build_docker_image.sh
@@ -4,17 +4,17 @@ echo "logging to ECR"
 aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 378652049675.dkr.ecr.eu-west-1.amazonaws.com
 
 echo "building docker image"
-docker build -t user2-web-app .
+docker build -t user5-web-app .
 
 
 
 echo "tagging the built docker image with the repo address"
-docker tag user2-web-app:latest 378652049675.dkr.ecr.eu-west-1.amazonaws.com/user2-web-app:latest
+docker tag user5-web-app:latest 378652049675.dkr.ecr.eu-west-1.amazonaws.com/user5-web-app:latest
 
 
 
 echo "pushing docker image to ECR"
-docker push 378652049675.dkr.ecr.eu-west-1.amazonaws.com/user2-web-app:latest
+docker push 378652049675.dkr.ecr.eu-west-1.amazonaws.com/user5-web-app:latest
 
 
 
